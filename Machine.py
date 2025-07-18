27ae49a423330495355de44745cdbcaeb90d8251
black-scythe0 <scytheblack991@gmail.com>
2025-07-18 22:59:11 +0530

    ignore object files

--- Machine.py
+++ Machine.py
@@ -14,7 +14,7 @@ PPLUS = enum()
 PMINUS = enum()
 PPOP = enum()
 PDUMP = enum()
-
+EMPTY = enum()
 
 
 def Push(x):
@@ -27,7 +27,8 @@ def Pop(x):
     return (PPOP, x)
 def Dump():
     return (PDUMP,)
-    
+def Empty():
+    return (EMPTY,)
     
 def interpreter(program):
     stack = []
@@ -46,7 +47,9 @@ def interpreter(program):
             stack.append(b-a)
             print(stack.pop())
         elif op[0] == 3:
-           print(list(op).pop())            
+           print(list(op).pop())
+        elif op[0] == 6:
+            remove( stack )           
 
 def compiler():
     assert False, "implimentation due"
@@ -56,8 +59,12 @@ def compiler():
 if __name__ == '__main__':
     program = [
         Push(90),
-        Push(10),
-        Minus(),
+#        Push(10),
+ #       Minus(),
+        Empty(),
+#        Push(0),
+        Push(80),
+        Plus(),
         Pop(10),
         Pop(10)
     ]
