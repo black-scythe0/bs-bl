824c59bd60e5ef3bb76785266d3f0bcdac38fd7b
black-scythe0 <scytheblack991@gmail.com>
2025-08-10 23:32:13 +0530

    bsbl.h

---
+++ src/bsbl.h
@@ -0,0 +1,10 @@
+
+#ifndef __BSBL__H__
+#define __BSBL__H__
+
+#define BSBL "BS-BL (black-scythe's Bang Language) by black-scythe0"
+#define BSBL_LICNESE "Copyright (c) 2025 black-scythe0"
+#define BSBL_AUTHORS "black-scythe0"
+
+
+#endif
